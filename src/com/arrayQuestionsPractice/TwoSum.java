c75f94a1689408ffa3fb7e9a373584736673a25a
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-28 17:15:20 +0530

    implemented two sum problem in array

--- src/com/arrayQuestionsPractice/TwoSum.java
+++ src/com/arrayQuestionsPractice/TwoSum.java
@@ -1,5 +1,24 @@
 package com.arrayQuestionsPractice;
 
-public class TwoSum {
+import java.util.HashMap;
 
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] arr = {11,3,7,9,14,2};
+        int target = 17;
+        //result array to store the result element
+        int[] res = new int[2];
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<arr.length; i++){
+            int second_No = target - arr[i];
+            if(map.containsKey(second_No)){
+                res[0] = map.get(second_No);
+                res[1] = i;
+                break;
+            }
+            map.put(arr[i],i);
+        }
+        System.out.println(res[0]+" "+res[1]);
+    }
 }
