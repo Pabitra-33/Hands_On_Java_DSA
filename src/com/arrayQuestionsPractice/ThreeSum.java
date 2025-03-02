dd7d8850726d27de0739c58bca891495f517f13a
Pabitra-33 <kumarpabitra345@gmail.com>
2025-03-02 10:36:44 +0530

    implemented three sum problem in array java

---
+++ src/com/arrayQuestionsPractice/ThreeSum.java
@@ -0,0 +1,8 @@
+package com.arrayQuestionsPractice;
+
+public class ThreeSum {
+    public static void main(String[] args) {
+        int[] arr = {11,3,7,9,14,2,6,5,8,3};
+        int target = 12;
+    }
+}
