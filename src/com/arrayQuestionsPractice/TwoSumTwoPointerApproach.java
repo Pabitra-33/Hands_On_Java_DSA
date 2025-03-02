f217cc6a567fac2ac7115983ba7068a1148482bb
Pabitra-33 <kumarpabitra345@gmail.com>
2025-03-02 10:15:20 +0530

    using two pointer approach implemented two sum problem in array

--- src/com/arrayQuestionsPractice/TwoSumTwoPointerApproach.java
+++ src/com/arrayQuestionsPractice/TwoSumTwoPointerApproach.java
@@ -8,9 +8,11 @@ public class TwoSumTwoPointerApproach {
         System.out.println(res[0]+" "+res[1]);
     }
     public static int[] twoSum(int[] nums, int target) {
+        //taking two pointer approach to solve
         for(int i=0; i<nums.length; i++) {
             for(int j=i+1; j<nums.length; j++){
-                if(nums[i]+nums[j] == target){
+                if(nums[i]+nums[j] == target) //if matches then returns a new array by taking these pointers as index
+                {
                     return new int[] {i, j};
                 }
             }
